615681a30efbb851f37b3b27ab396c8c7ae79b13
Gabriel <gabrieldewes>
2016-06-10 02:10:47 -0300

    só vai

--- home/app/application/views/template/view_footer.php
+++
@@ -1,9 +0,0 @@
-
-	</div>
-		<footer class="panel-footer">
-			<p>
-				&copy; <script type="text/javascript"> document.write(new Date().getFullYear()); </script> &middot; Gabriel Dewes
-			</p>
-		</footer>
-	</body>
-</html>
