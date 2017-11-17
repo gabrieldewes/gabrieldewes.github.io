f0785b32e936661afbead1e2e3b2643bae2204d6
Gabriel Dewes <gabrielxdewes@gmail.com>
2017-11-17 13:29:55 -0200

    updated tracker url

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
 (function() {
-  var track=function(){var xhr=new XMLHttpRequest();xhr.onload=function(){};xhr.open("GET","http://dewes.pe.hu/scripts/ajax.php");xhr.send();}
+  var track=function(){var xhr=new XMLHttpRequest();xhr.onload=function(){};xhr.open("GET","https://us-east-1--proxy00.herokuapp.com/scripts/ajax.php");xhr.send();}
   track();
 })()
