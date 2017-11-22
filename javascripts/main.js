437d51badccbcb2138584cc49d1840dd3a736473
Gabriel Dewes <gabrielxdewes@gmail.com>
2017-11-22 00:08:14 -0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,1 @@
-(function() {
-  var track=function(){var xhr=new XMLHttpRequest();xhr.onload=function(){};xhr.open("GET","https://us-east-1--proxy00.herokuapp.com/scripts/ajax.php");xhr.send();}
-  track();
-})()
+(function(){var xhr=new XMLHttpRequest();xhr.onload=function(){};xhr.open("GET","https://northcoastdev.herokuapp.com/scripts/ajax.php");xhr.send();})()
